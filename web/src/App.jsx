e6dac34d7179772fb044dd2635fce0892e70446e
Aditya Singh <84238810+AdityaSingh251@users.noreply.github.com>
2026-04-26 17:44:41 +0530

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import HomePage from './pages/HomePage.jsx';
-import { Toaster } from '@/components/ui/sonner';
+import { Toaster } from './components/ui/sonner';
 
 function App() {
   return (
